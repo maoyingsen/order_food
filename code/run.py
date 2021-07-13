d576d1e9c663b9fa1adba7489372f9e65c7652b0
maoyingsen <yingsenmao@163.com>
2021-07-13 16:09:50 +0800

    update

---
+++ code/run.py
@@ -0,0 +1,24 @@
+test = [
+	{
+        'cbbh': '1 ', 'sort': '主食', 'code': '1000', 'name': '米饭', 'price':
+	 	Decimal('1.00'), 'gx': '普通', 'rl': Decimal('0.00'), 'dbz': Decimal('0.00'), 
+        'ts': Decimal('0.00'), 'dcsl': Decimal('999.00'), 'zh': '01'
+    }, 
+	{
+        'cbbh': '1 ', 'sort': '主食', 'code': '1001', 'name': '馒头', 'price': 			
+	 	Decimal('1.00'), 'gx': '普通', 'rl': Decimal('0.00'), 'dbz': Decimal('0.00'), 
+        'ts': Decimal('0.00'), 'dcsl': Decimal('999.00'), 'zh':'01'
+    },
+    {
+        'cbbh': '2 ', 'sort': '副食', 'code': '2004', 'name': '黄豆芽粉丝', 'price': Decimal('1.00'), 
+        'gx': '普通', 'rl': Decimal('0.00'), 'dbz': Decimal('0.00'), 	
+        'ts':Decimal('0.00'), 'dcsl': Decimal('999.00'), 'zh': '03'
+    },
+    {
+        'cbbh': '4 ', 'sort': '汤粥', 'code': '3006', 'name': '苹果汁',
+		'price': Decimal('1.00'), 'gx': '清流', 'rl': Decimal('0.00'), 'dbz': 	
+        Decimal('0.00'), 'ts': Decimal('0.00'), 'dcsl': Decimal('999.00'), 'zh': '01'
+    }
+]
+
+print(test)
